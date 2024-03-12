5f02313dfb588b8ba083ec2174bb9fa563a9b665
ytop <tomilovyury@mail.ru>
2024-03-12 17:12:30 +0300

    Bugs fixed

--- app/src/main/java/com/mtuci/vki/main/MainScreen.kt
+++ app/src/main/java/com/mtuci/vki/main/MainScreen.kt
@@ -1,17 +1,13 @@
 package com.mtuci.vki.main
 
-import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.compose.collectAsLazyPagingItems
-import com.mtuci.vki.MainActivity
 import com.mtuci.vki.viewmodel.ProductViewModel
 
 @Composable
 fun MainScreen(){
-    val activity = LocalContext.current as MainActivity
-
-    val viewModel: ProductViewModel by activity.viewModels()
+    val viewModel: ProductViewModel = viewModel()
     val pagingData = viewModel.products.collectAsLazyPagingItems()
 
     MainContent(pagingData)
